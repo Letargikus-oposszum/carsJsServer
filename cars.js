0a37188150ef9d6e8d611ccfc1804f86b56f28a4
Letargikus-oposszum <144096062+Letargikus-oposszum@users.noreply.github.com>
2025-03-03 11:33:43 +0100

    Add files via upload

--- cars.js
+++ cars.js
@@ -1,8 +1,49 @@
-const cars =[
-    {brand:"Audi",model:"A4",year:2022},
-    {brand:"Pagani",model:"Zonda Queen Roadster",year:2022},
-    {brand:"Citroen",model:"C4",year:2022},
-    {brand:"BYD",model:"Sealion",year:2022},
-]
+import express from 'express'
+import cars from '../data/cars.js'
 
-export default cars
+const router = express.Router()
+
+router.get('/', (req, res) =>{
+    res.status(200).json(cars)
+})
+
+router.get('/:id', (req, res) =>{
+    const id = req.params.id
+    if(id < 0 || id > cars.length){
+        return res.json({message:"Car not found"})
+    }
+    res.status(200).json(cars[id])
+})
+
+router.post('/', (req, res) =>{
+    const {brand,model,year} = req.body
+    if (!brand||!model||!year){
+        return res.status(404).json({message: "Missing data"})
+    }
+    const newCar = {brand,model,year};
+    cars.push(newCar)
+    res.status(201).json(newCar)
+})
+
+router.put('/:id', (req, res) =>{
+    const id = req.params.id
+    if(id < 0 || id > cars.length){
+        return res.json({message:"Car not found"})
+    }
+    const {brand,model,year} = req.body
+    if (!brand||!model||!year){
+        return res.status(404).json({message: "Missing data"})
+    }
+    res.status(200).json(cars[id])
+})
+
+router.delete('/:id', (req, res) =>{
+    const id = req.params.id
+    if(id < 0 || id > cars.length){
+        return res.json({message:"Car not found"})
+    }
+    cars.splice(id, 1)
+    res.status(200).json({message:"Car has been successfully deleted!"})
+})
+
+export default router
